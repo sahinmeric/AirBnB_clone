17d506a970111bd6ad5296dbbacb9395756a8cab
root <guzchelsea24@gmail.com>
2022-03-01 14:15:28 -0800

    update ex3 n 4

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,20 @@ from datetime import datetime
 
 class BaseModel:
     """Base model class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at #this should be executed when instance first time created
-    
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        fmt = '%Y-%m-%dT%H:%M:%S.%f'
+                        setattr(self, key, datetime.strptime(value, fmt))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at #this should be executed when instance first time created
 
     def __str__(self) -> str:
         """String presentation of BaseModel class"""
@@ -26,4 +34,10 @@ class BaseModel:
     
     def to_dict(self):
        """returns a dictionary containing all keys/values of __dict__ of the instance"""
-       return self.__dict__
+       dict_upd = {"__class__": self.__class__.__name__}
+       for key in self.__dict__:
+            if key == 'created_at' or key == 'updated_at':
+                dict_upd[key] = self.__dict__[key].isoformat()
+            else:
+                dict_upd[key] = self.__dict__[key]
+            return dict_upd
