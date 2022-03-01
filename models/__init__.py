24f314cc4be07ea8543cbe6bcc408d251c44cd24
sahinmeric <sahin.meric86@gmail.com>
2022-03-01 19:25:36 -0300

    storage

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+creates a unique FileStorage instance
+"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,30 @@
 """
 This script has Base model class
 """
-
+from pyexpat import model
+import models
 import uuid
 from datetime import datetime
+format = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
     """Base model class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at #this should be executed when instance first time created
-    
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or "updated_at":
+                    setattr(self, k, datetime.strptime(v,format))
+                elif k == "__class__":
+                    continue
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at #this should be executed when instance first time created
+        models.storage.new(self)
 
     def __str__(self) -> str:
         """String presentation of BaseModel class"""
@@ -23,6 +34,7 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
     
     def to_dict(self):
        """returns a dictionary containing all keys/values of __dict__ of the instance"""
