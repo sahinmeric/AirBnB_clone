828b3c4f2ed56d67fc38123277f9e8676fa852e1
sahinmeric <sahin.meric86@gmail.com>
2022-03-03 14:57:58 -0300

    console 0.0.1

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script containts the entry point of
+the command interpreter
+"""
+
+import cmd
+
+from click import prompt
+
+class HBNBCommand(cmd.Cmd):
+    """ Command interpreter"""
+    intro = ""
+    prompt = "(hbtn) "
+    file = None
+
+    def do_EOF(self, arg):
+        """EOF"""
+        return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
