c4416823690cb1f5339b44da12b1999a373431a0
sahinmeric <sahin.meric86@gmail.com>
2022-03-05 14:18:17 -0300

    empty line handle

--- console.py
+++ console.py
@@ -4,14 +4,16 @@ This script containts the entry point of
 the command interpreter
 """
 
+
 import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
     """ Command line interpreter"""
-    intro = ""
     prompt = "(hbtn) "
-    file = None
+    class_list = ['BaseModel', 'User', 'State', 'City',
+                  'Amenity', 'Place', 'Review']
 
     def do_EOF(self, arg):
         """EOF Quits from the program"""
@@ -20,6 +22,20 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
+
+    def emptyline(self):
+        """ empty line handle"""
+        pass
+
+    def do_create(self, arg):
+        """ creates an instance of given class"""
+        if arg:
+            if arg in self.class_list:
+                print("Exists")  # TODO
+            else:
+                print("class doesn't exist")
+        else:
+            print("class name missing")
 
 
 if __name__ == '__main__':
